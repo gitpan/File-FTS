5ecf197c3b659a2f2bbbd3b14dbd2bdb4d0e3a9c
Shlomi Fish <shlomif@shlomifish.org>
2002-12-22 11:38:59 +0000

    Import of SHLOMIF/File-FTS-0.02 from CPAN.
    gitpan-cpan-distribution: File-FTS
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHLOMIF/File-FTS-0.02.tar.gz
    gitpan-cpan-author:       SHLOMIF
    gitpan-cpan-maturity:     released

--- FTS.pm
+++ FTS.pm
@@ -1,48 +1,53 @@
 package File::FTS;
+
 use strict;
-use Symbol;
+
 use File::Spec;
+
 use vars qw($VERSION);
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 sub new {
-	my ($class, $base) = @_;
-	bless [{'dir' => $base}], $class;
+    my ($class, $base) = @_;
+    bless [{'dir' => $base}], $class;
 }
 
 sub Dive {
-	my $self = shift;
-	my $this = $self->[-1];
-	return undef unless $this;
-	my $fd;
-	unless ($fd = $this->{'fd'}) {
-		$fd = $this->{'fd'} = gensym;
-		opendir($fd, $this->{'dir'});
-	}
-	my $item = readdir($fd);
-	if ($item eq "." || $item eq "..") {
-		return $self->Dive;
-	}
-	unless (defined($item)) {
-		closedir($fd);
-		pop @$self;
-		return $self->Dive;
-	}
-	my $name = File::Spec->catfile($this->{'dir'}, $item);
-	unless (-d $name) {
-		return $name;
-	}
-	if (-l $name) {
-		return $self->Dive;
-	}
-	push(@$self, {'dir' => $name});
-	return $self->Dive;
+    my $self = shift;
+    my $this = $self->[-1];
+    return undef unless $this;
+    my $files;
+    unless ($files = $this->{'files'}) {
+        local(*DIR);
+        opendir(DIR, $this->{'dir'});
+        my @files_list = readdir(DIR);
+        $files = $this->{'files'} = \@files_list;
+        closedir(DIR);
+    }
+    my $item = shift(@$files);
+    unless (defined($item)) {
+        pop @$self;
+        return $self->Dive;
+    }
+    if ($item eq "." || $item eq "..") {
+        return $self->Dive;
+    }    
+    my $name = File::Spec->catfile($this->{'dir'}, $item);
+    unless (-d $name) {
+        return $name;
+    }
+    if (-l $name) {
+        return $self->Dive;
+    }
+    push(@$self, {'dir' => $name});
+    return $name;
+    # return $self->Dive;
 }
 
 1;
 __END__
-# Below is the stub of documentation for your module. You better edit it!
+
 
 =head1 NAME
 
@@ -51,9 +56,9 @@ FTS - Perl extension for File Traversing System
 =head1 SYNOPSIS
 
   use FTS;
-  $fts = new FST("/usr/local");
+  $fts = new FTS("/usr/local");
   while ($file = $fts->Dive) {
-  	print "$file\n";
+      print "$file\n";
   }
 
 =head1 DESCRIPTION
@@ -65,7 +70,10 @@ Inspired by the BSD fts library.
 
 =head1 AUTHOR
 
-Ariel Brosh, B<schop@cpan.org>
+Original Author: Ariel Brosh, L<http://search.cpan.org/author/SCHOP/> (Ariel
+has unexpectdly passed away and this module has been adopted by someone else)
+
+Current Maintainer: Shlomi Fish, B<shlomif@cpan.org>
 
 =head1 SEE ALSO
 
